d3e0370ef1bdf74f96f3a582421645ad98aa8adc
Peter Danenberg <peter@factual.com>
2011-12-29 14:38:16 -0800

    Useless project

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject napa "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [clojure-hadoop "1.3.1-SNAPSHOT"]
+                 [org.clojure/data.json "0.1.1"]]
+  :dev-dependencies [[debug "1.0.0-SNAPSHOT"]])

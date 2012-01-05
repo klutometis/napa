6756cdc63de3e01e5aa54baa96fd61abb956661d
Peter Danenberg <peter@factual.com>
2012-01-05 13:19:47 -0800

    Need to create the graph from the tree first.

--- project.clj
+++ project.clj
@@ -2,5 +2,10 @@
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [clojure-hadoop "1.3.1-SNAPSHOT"]
-                 [org.clojure/data.json "0.1.1"]]
+                 [org.clojure/data.json "0.1.1"]
+                 [factual/vineyard-java-driver "1.4.3-SNAPSHOT"]
+                 [cadr "1.0.1-SNAPSHOT"]]
+  :repositories {"factual" "http://maven.corp.factual.com/nexus/content/groups/public"
+                 "releases" "http://maven.corp.factual.com/nexus/content/repositories/releases"
+                 "snapshots" "http://maven.corp.factual.com/nexus/content/repositories/snapshots"}
   :dev-dependencies [[debug "1.0.0-SNAPSHOT"]])

dcd6bde3b32fc5ef4c17dc363c878008d60c21af
Peter Danenberg <peter@factual.com>
2012-01-05 14:47:57 -0800

    Circularity, anyone?

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
                  [clojure-hadoop "1.3.1-SNAPSHOT"]
                  [org.clojure/data.json "0.1.1"]
                  [factual/vineyard-java-driver "1.4.3-SNAPSHOT"]
-                 [cadr "1.0.1-SNAPSHOT"]]
+                 [cadr "1.0.1-SNAPSHOT"]
+                 [lambda "1.0.2-SNAPSHOT"]]
   :repositories {"factual" "http://maven.corp.factual.com/nexus/content/groups/public"
                  "releases" "http://maven.corp.factual.com/nexus/content/repositories/releases"
                  "snapshots" "http://maven.corp.factual.com/nexus/content/repositories/snapshots"}

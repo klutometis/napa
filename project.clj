144254c3e61d919d542371f016c43226337d4381
Peter Danenberg <peter@factual.com>
2012-01-06 12:47:07 -0800

    Actually run the tasks.

--- project.clj
+++ project.clj
@@ -3,9 +3,11 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [clojure-hadoop "1.3.1-SNAPSHOT"]
                  [org.clojure/data.json "0.1.1"]
-                 [factual/vineyard-java-driver "1.4.3-SNAPSHOT"]
                  [cadr "1.0.1-SNAPSHOT"]
-                 [lambda "1.0.2-SNAPSHOT"]]
+                 [lambda "1.0.2-SNAPSHOT"]
+                 [factual/vineyard-hadoop "1.2.3-SNAPSHOT"]
+                 ;; [factual/vineyard-java-driver "1.4.3-SNAPSHOT"]
+                 ]
   :repositories {"factual" "http://maven.corp.factual.com/nexus/content/groups/public"
                  "releases" "http://maven.corp.factual.com/nexus/content/repositories/releases"
                  "snapshots" "http://maven.corp.factual.com/nexus/content/repositories/snapshots"}

2f8f9b66d4372b0cccec3ff8d81e2a93bac5cf00
Peter Danenberg <peter@factual.com>
2012-01-16 14:35:13 -0800

    Why do we need to do this?

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [clojure-hadoop "1.3.1-SNAPSHOT"]
-                 [org.clojure/data.json "0.1.1"]]
+                 [org.clojure/data.json "0.1.1"]
+                 [factual/hadoop-extraction-workflow "0.1-SNAPSHOT"]]
   :dev-dependencies [[debug "1.0.0-SNAPSHOT"]])

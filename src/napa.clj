78f2cd5276972f22772b9eaffa6b04ddcc99dde8
Peter Danenberg <pcd@roxygen.org>
2012-01-31 23:31:46 -0800

    Clean up project spec; first cut at package

--- src/napa.clj
+++ src/napa.clj
@@ -1,23 +1,10 @@
 (ns napa
-  (:use ))
-
-(use 'add-classpath.core)
-
-(add-classpath "lib/*")
-(add-classpath "lib/dev/*")
-
-(use 'debug.core)
-(use 'lambda.core)
-(use 'cadr.core)
-(use 'clojure.test)
-
-(import '(vineyard.hadoop MapReduceTask)
-        '(vineyard.shell ShellTask)
-        '(vineyard Task
-                   TaskQueue)
-        '(hewtoy MD5AndPayloadJoiner
-                 UUIDToPayloadMapper
-                 UUIDToMD5Mapper))
+  (:use [debug]
+        [cadr]
+        [clojure.test])
+  (:import (vineyard.hadoop MapReduceTask)
+           (vineyard.shell ShellTask)
+           (vineyard Task TaskQueue)))
 
 (defn make-map-reduce-task [name class input output]
   (doto (MapReduceTask.)
@@ -30,7 +17,7 @@
   (doto (ShellTask.)
     (.setInitData ShellTask/SHELL_CMD_KEY path)))
 
-(defλ tree->graph [tree]
+(defn tree->graph [tree]
   (let [child->parents (transient {})
         parent->children (transient {})]
     (letfn [(plumb! [parents tree]
@@ -67,55 +54,19 @@
       {:child->parents (persistent! child->parents)
        :parent->children (persistent! parent->children)})))
 
-#_(defλ run-tree [tree]
-    (let [graph (tree->graph tree)
-          tasks (map :task (topological-sort graph))]
-      (.runAll (TaskQueue. *default-host* *default-port* *default-resource*)
-               (car tasks))))
-
-(import '(hewtoy UUIDToMD5Mapper
-                 UUIDToMD5Mapper
-                 MD5AndPayloadJoiner))
-
-;;; These procedures are distinct from the DSL that invokes them.
-(def join-md5-and-payload
-  (make-map-reduce-task
-   "join-md5-and-payload"
-   MD5AndPayloadJoiner
-   "uuid-to-payload,uuid-to-md5"
-   "md5-and-payload"))
-
-(def map-uuid-to-md5
-  (make-map-reduce-task
-   "map-uuid-to-md5"
-   UUIDToMD5Mapper
-   "md5-to-uuid"
-   "uuid-to-md5"))
-
-(def map-uuid-to-payload
-  (make-map-reduce-task
-   "map-uuid-to-payload"
-   UUIDToPayloadMapper
-   "md5-to-uuid"
-   "uuid-to-payload"))
-
-(def make-uuid-md5s
-  (make-shell-task
-   "./make-md5-to-uuid.sh"))
-
-(defλ task-table [tasks]
-  (reduce (λ [table task] (assoc table task (eval task)))
+(defn task-table [tasks]
+  (reduce (fn [table task] (assoc table task (eval task)))
           {}
           tasks))
 
-(defλ build-job [parent->children root tasks]
+(defn build-job [parent->children root tasks]
   (let [queue (atom (clojure.lang.PersistentQueue/EMPTY))
         visited (transient #{})]
-    (swap! queue (λ [queue] (conj queue root)))
+    (swap! queue (fn [queue] (conj queue root)))
     (conj! visited root)
     (while (not (empty? (deref queue)))
       (let [parent (peek (deref queue))]
-        (swap! queue (λ [queue] (pop queue)))
+        (swap! queue (fn [queue] (pop queue)))
         (let [children (get parent->children parent)]
           (doseq [child children]
             (if (not (get visited child))
@@ -123,24 +74,19 @@
                 (.addFirstly (get tasks parent)
                              (get tasks child))
                 (conj! visited child)
-                (swap! queue (λ [queue] (conj queue child)))))))))
+                (swap! queue (fn [queue] (conj queue child)))))))))
     (get tasks root)))
 
 (def ^:dynamic *default-host* "task07")
 (def ^:dynamic *default-port* 8081)
 (def ^:dynamic *default-resource* "test")
 
-(let [{:keys [child->parents parent->children]}
-      (tree->graph
-       '(join-md5-and-payload
-         (map-uuid-to-md5
-          make-uuid-md5s)
-         (map-uuid-to-payload
-          make-uuid-md5s)))
-      tasks (task-table '(join-md5-and-payload
-                          map-uuid-to-md5
-                          map-uuid-to-payload
-                          make-uuid-md5s))]
-  (let [root (build-job parent->children 'join-md5-and-payload tasks)]
-    (.runAll (TaskQueue. *default-host* *default-port* *default-resource*)
-             root)))
+(defn tree->tasks [tree]
+  (distinct (flatten tree)))
+
+(defn run-tree [tree root]
+  (let [{:keys [child->parents parent->children]} (tree->graph tree)
+        tasks (tree->tasks tree)]
+    (let [root (build-job parent->children root tasks)]
+      (.runAll (TaskQueue. *default-host* *default-port* *default-resource*)
+               root))))
